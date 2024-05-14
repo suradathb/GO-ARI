bec35039119eea4f705bbaade103e45b307f10ec
11429651 <Suradath.Bangnikrai@krungsrisecurities.com>
2024-05-14 16:52:30 +0700

    Update Go ARI

---
+++ models/book.go
@@ -0,0 +1,14 @@
+// models/book.go
+package models
+
+type Book struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+var Books = []Book{
+	{ID: "1", Title: "Go Programming", Author: "John Doe", Year: "2021"},
+	{ID: "2", Title: "Advanced Go", Author: "Jane Doe", Year: "2022"},
+}
---
+++ routes/book_routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"go-ari/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RegisterBookRoutes(r *gin.Engine) {
+	api := r.Group("/api/v1")
+	{
+		api.GET("/books", controllers.GetBooks)
+		api.GET("/books/:id", controllers.GetBook)
+		api.POST("/books", controllers.CreateBook)
+		api.PUT("/books/:id", controllers.UpdateBook)
+		api.DELETE("/books/:id", controllers.DeleteBook)
+	}
+}
